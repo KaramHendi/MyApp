bfd78b50c76d1521bc9ef67684b911ca4cee704e
KARAMHENDIZD <karamhendizd10@gmail.com>
2024-05-26 09:36:23 +0300

    My first version

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
-    int a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
